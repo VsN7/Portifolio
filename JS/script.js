1d0a22a5a252884abc9c95f2eeeec088b6ab974f
Vitor Santos <vitor7_sn@hotmail.com>
2019-04-16 17:34:07 -0300

    Ajustando scroll do site

--- JS/script.js
+++ JS/script.js
@@ -19,8 +19,11 @@ const debounce = function(func, wait, immediate){
 
 
 const target = document.querySelectorAll('[data-anime]');
+const naigation = document.querySelectorAll('[data-target]');
 const animationClass = 'animate';
-
+const animationNav = 'animateNav';
+let i=0;
+let primeiro =0;
 function animeScroll(){
     
     const windowTop = window.pageYOffset + ((window.innerHeight * 3)/4);
@@ -28,15 +31,33 @@ function animeScroll(){
     target.forEach(function(element){
         
         if((windowTop) > element.offsetTop){
-            
+
+            if(primeiro ==0){
+                i=windowTop;
+                primeiro ==1;
+            }
             element.classList.add(animationClass);
         }else{
             element.classList.remove(animationClass);
+
+
+        }
+        if(windowTop> 900){
+            
+
+            naigation.forEach(function(element){
+                element.classList.add(animationNav);
+            
+        })
+        }else{
+            naigation.forEach(function(element){
+                element.classList.remove(animationNav);
+            
+        })
         }
         
     })
 }
-
 animeScroll();
 
 if(target.length){
